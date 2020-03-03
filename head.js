aa24c489b5a40bedc36bf0a9cbce7d4c54d94b22
Brad Ruud <bradruud@gmail.com>
2020-03-03 03:01:05 +0000

    Adding Head file

---
+++ head.js
@@ -0,0 +1,15 @@
+const head = function(a) {
+  let first = a[0];
+  return first;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
