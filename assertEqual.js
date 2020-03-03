a0574dfa0f1987f20e34193ae1c2999175dff67a
Brad Ruud <bradruud@gmail.com>
2020-03-03 00:52:46 +0000

    adding assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍👍👍 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
