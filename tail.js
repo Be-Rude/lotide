fc5ad16f2fadd05b693653902a2c558aafea9103
Brad Ruud <bradruud@gmail.com>
2020-03-03 04:11:30 +0000

    adding tail file

---
+++ tail.js
@@ -0,0 +1,19 @@
+const tailArray = [];
+
+const tail = function(words) {
+  for (let i = 1; i < words.length; i++) {
+    tailArray.push(words[i]);
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
