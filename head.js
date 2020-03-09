6c413edfffb67d99f6b6ab5fe2860293667146ae
Brad Ruud <bradruud@gmail.com>
2020-03-09 19:55:21 +0000

    adding 4 new files including headTest.js and assertEqualTest.js

--- head.js
+++ head.js
@@ -1,15 +1,20 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(a) {
   let first = a[0];
   return first;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function() {
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// //   if (actual === expected) {
+// //     console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+// //   } else {
+// //     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+// //   }
+// };
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
